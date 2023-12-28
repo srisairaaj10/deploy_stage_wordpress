d12fc34e36411a7c795ea2300b230e1a786bbd7f
srisairaaj10 <85604853+srisairaaj10@users.noreply.github.com>
2023-12-29 01:41:34 +0530

    Add files via upload

---
+++ word-press-3-new/src/infrastructure/variables.tf
@@ -0,0 +1,32 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "config" {
+  type    = string
+  default = "~/.aws/config"
+}
+
+variable "credentials" {
+  type    = string
+  default = "~/.aws/credentials"
+}
+
+variable "my_ip" {
+  description = "ip address"
+  type        = string
+}
+
+variable "zone_one" {
+  description = "region"
+  type        = string
+  default = "ap-south-1a"
+  
+}
+
+variable "zone_two" {
+  description = "region"
+  type        = string
+  default = "ap-south-1b"
+}
